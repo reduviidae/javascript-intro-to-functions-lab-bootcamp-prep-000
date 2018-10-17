8df0a85c64593e9278c0c6e31620d7425b6cb4d6
Wil Cook <wil.cook.is@gmail.com>
2018-10-17 19:24:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,10 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === lowercase) {
-    return "I can't hear you!";
+    return "I can't hear you!"
   } else if (string === uppercase) {
-    return "YES INDEED!";
-  } else (string === "I love you, Grandma!") {
-    return "I love you, too.";
-  }
+    return "YES INDEED!"
 }
